89798228463d5a79fb6174c2e2952316ddf6188d
hnsia <44498178+hnsia@users.noreply.github.com>
2024-09-11 02:03:27 +0800

    feat: add request validation

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/hnsia/micro-oms/common"
@@ -28,8 +29,37 @@ func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.client.CreateOrder(r.Context(), &api.CreateOrderRequest{
+	if err := validateItems(items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	o, err := h.client.CreateOrder(r.Context(), &api.CreateOrderRequest{
 		CustomerID: customerID,
 		Items:      items,
 	})
+	if err != nil {
+		common.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	common.WriteJSON(w, http.StatusOK, o)
+}
+
+func validateItems(items []*api.ItemsWithQuantity) error {
+	if len(items) == 0 {
+		return errors.New("items must have at least one item")
+	}
+
+	for _, i := range items {
+		if i.ID == "" {
+			return errors.New("item ID is required")
+		}
+
+		if i.Quantity <= 0 {
+			return errors.New("items must have a valid quantity")
+		}
+	}
+
+	return nil
 }
